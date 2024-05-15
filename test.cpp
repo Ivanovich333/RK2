4aa404174a4c92009f01bfd1a93038e41efe8bca
Ivanovich333 <vizgalovyaroslav@gmail.com>
2024-05-16 02:27:56 +0300

    Initial commit

--- test.cpp
+++ test.cpp
@@ -1,12 +1,26 @@
 #include <gtest/gtest.h>
 #include "implementation.h"
 
-TEST(BridgePatternTest, ConcreteImplementationATest) {
+TEST(BridgePatternTest, ObjectCreationTest) {
+    ConcreteImplementationA impl;
+    EXPECT_NO_THROW();
+}
+
+TEST(BridgePatternTest, OperationMethodTest) {
     ConcreteImplementationA impl;
     testing::internal::CaptureStdout();
     impl.Operation();
     std::string output = testing::internal::GetCapturedStdout();
     EXPECT_EQ(output, "ConcreteImplementationA Operation\n");
+}
+
+TEST(BridgePatternTest, PolymorphismTest) {
+    Implementation* impl = new ConcreteImplementationA();
+    testing::internal::CaptureStdout();
+    impl->Operation();
+    std::string output = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(output, "ConcreteImplementationA Operation\n");
+    delete impl;
 }
 
 int main(int argc, char **argv) {
