4d8c428bebfb901ad7e0f37ddbd5208d57d37c6a
Ivanovich333 <vizgalovyaroslav@gmail.com>
2024-05-16 02:02:49 +0300

    Initial commit

---
+++ test.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+#include "implementation.h" // Замените на актуальный заголовочный файл
+
+TEST(BridgePatternTest, Test1) {
+    // Ваши тесты здесь
+    EXPECT_EQ(1, 1);
+}
+
+TEST(BridgePatternTest, Test2) {
+    // Ваши тесты здесь
+    EXPECT_EQ(2, 2);
+}
+
+TEST(BridgePatternTest, Test3) {
+    // Ваши тесты здесь
+    EXPECT_EQ(3, 3);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
